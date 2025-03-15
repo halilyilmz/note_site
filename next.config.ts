135e9ab7ba5d58502cacba29237948b66c348c98
ibrahim halil <halilhyl83@gmail.com>
2025-03-15 16:06:04 +0300

    eslint rules removed

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    crossOrigin: "use-credentials"
+    crossOrigin: "use-credentials",
+    eslint: {
+        ignoreDuringBuilds: true, // ESLint hatalarını build sırasında yok sayar
+    },
 };
 
 module.exports = nextConfig;
